597f3e4d9f644809c48cf26d08e3da99f470f6cb
a87Nozaki <dress222@MacBook-Air.local>
2020-11-20 02:19:13 +0900

    テストコード間違い修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname presence: true
-  validates :email, uniquess: true
-  validates :passward, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'は英字と数字の両方を含めて設定してください' }
-  validates :first_name presence: true, format: { with: /\A[ぁ-ん ァ-ン 一-龥]+\z/, messa: 'は全角文字を使用してください' }
-  validates :last_name presence: true, format: { with: /\A[ぁ-ん ァ-ン 一-龥]+\z/, messa: 'は全角文字を使用してください' }
-  validates :first_name_kana presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください'}
-  validates :last_name_kana presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください'}
-  validates :birthday presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'は英字と数字の両方を含めて設定してください' }
+
+  with_options presence: true, format:  { with: /\A[ぁ-ん ァ-ン 一-龥]+\z/, message: 'は全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください'} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,44 @@ require 'rails_helper'
 
 describe User do
   before do
-    @user = FactoryBot.build(:user）
+    @user = FactoryBot.build(:user)
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録できる場合' do
-      it 'nickname,email,password_confimation,first_name,last_name,first_name_kana,last_name_kana,birthdayがあれば新規登録できる' do
+    context '新規登録できるとき' do
+      it 'nickname,email,password,password_confimation,first_name,last_name,first_name_kana,last_name_kana,birthdayがあれば新規登録できる' do
         expect(@user).to be_valid
       end
+
+
     end
 
-    context '新規登録ができない場合' do
+    context '新規登録ができないとき' do
       it 'nicknameが空だと登録できない' do
-        @user.nickname = null
+        @user.nickname = nil
         @user.valid?
         expect(@user.errors.full_messages).to include 'ニックネームを入力してください'
       end
 
+      it 'emailが空の場合は登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'メールアドレスを入力してください'
+      end
+
+      it '存在するemailでは登録できない' do
+        anther_user = FactoryBot.build(:user)
+        anther_user.email
+        anther_user.valid?
+        expect(anther_user.errors.full_messages).to include 'そのメールアドレスはすでに存在しています'
+      end
+
       it 'emailに＠が存在しない場合は登録できない' do
-        @user.email = 'nozakiyuki'
+        @user.email = 'testtest.com'
         @user.valid?
         expect(@user.errors.full_messages).to include 'メールアドレスは不正な値です'
       end
+
 
       it 'passwordが空では登録できない' do
         @user.password = nil
@@ -33,28 +49,28 @@ describe User do
 
       it 'passwordが5文字以下では登録できない' do
         @user.password = 'abc12'
-        @user.password_confimation = 'abc12'
+        @user.password_confirmation = 'abc12'
         @user.valid?
         expect(@user.errors.full_messages).to include 'パスワードは6文字以上で入力してください'
       end
 
       it 'passwordが英字のみでは登録できない' do
         @user.password = 'abcdef'
-        @user.password_confimation = 'abcdef'
+        @user.password_confirmation = 'abcdef'
         @user.valid?
         expect(@user.errors.full_messages).to include 'パスワードは英字と数字の両方を含めて設定してください'
       end
 
       it 'passwordが数字のみでは登録できない' do
         @user.password = '123456'
-        @user.password_confimation = '123456'
+        @user.password_confirmation = '123456'
         @user.valid?
         expect(@user.errors.full_messages).to include 'パスワードは英字と数字の両方を含めて設定してください'
       end
 
       it 'password_confirmationが空では登録できない' do
         @user.password = 'abc123'
-        @user.password_confimation = ''
+        @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include '確認用パスワードとパスワードが一致しません'
       end
@@ -65,7 +81,7 @@ describe User do
         expect(@user.errors.full_messages).to include '姓名を入力してください'
       end
 
-      it 'first_nameが全角(漢字,ひらがな,カタナカ)でない場合登録できない' do
+      it 'first_nameが全角(漢字,ひらがな,カタカナ)でない場合登録できない' do
         @user.first_name = 'nozaki'
         @user.valid?
         expect(@user.errors.full_messages).to include '姓名は全角で入力してください'
@@ -74,7 +90,7 @@ describe User do
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
-        expect(p@user.errors.full_messages).to include '名前を入力してください'
+        expect(@user.errors.full_messages).to include '名前を入力してください'
       end
       
       it 'last_nameが全角(漢字,ひらがな,カタカナ)でない場合登録できない' do
